1039e10af72163f8abe170593da3c7d85e946fa7
Your Name <you@example.com>
2024-06-05 21:24:01 -0300

    add hardhat support

---
+++ services-init-config/blockscout/docker-compose/services/hardhat/hardhat.config.local.js
@@ -0,0 +1,54 @@
+require("@nomiclabs/hardhat-ethers");
+const fs = require("fs");
+// const { bytecode } = require("./Create2Deployer.json");
+
+// task(
+//   "deploy",
+//   "Deploys contract and writes address to ./.contract",
+//   async (taskArgs, hre) => {
+//     // We get the contract to deploy
+//     const abi = []; // stubbed out since it is unecessary here
+//     const NA = await hre.ethers.getContractFactory(abi, bytecode);
+//     const na = await NA.deploy();
+
+//     // Await deployment
+//     await na.deployed();
+
+//     // Write file
+//     fs.writeFileSync("./.contract", na.address);
+//   },
+// );
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+    solidity: {
+        version: '0.8.24',
+        settings: {
+            evmVersion: "london",
+            optimizer: {
+                enabled: true,
+                runs: 1000,
+            },
+        },
+    },
+    defaultNetwork: 'hardhat',
+    initialBaseFeePerGas: 0,
+    networks: {
+        hardhat: {
+            hardfork: "london",
+            initialBaseFeePerGas: 0,
+            chainId: 381660001,
+            allowUnlimitedContractSize: true,
+            loggingEnabled: true,
+            gasPrice: 0,
+            gas: 0,
+            gasMultiplier: 0,
+            accounts: [{ privateKey: `0xb584831ccaa00a44d3f76bd49743e2bc0caf5795376c6556bbd77bb1c6ebd4bf`, balance: "90000000000000000000000" }]
+        }
+    }
+}
--- services-init-config/smartcontracts/hardhat.config.ts
+++ services-init-config/smartcontracts/hardhat.config.ts
@@ -17,11 +17,19 @@ const config: HardhatUserConfig = {
   defaultNetwork: 'firefly',
   networks: {
     firefly: {
-      url: 'http://127.0.0.1:8545',
+      chainId: 381660001,
+      url: 'http://localhost:7545',
       accounts: [`0xb584831ccaa00a44d3f76bd49743e2bc0caf5795376c6556bbd77bb1c6ebd4bf`],
+      loggingEnabled: true
     },
     hardhat: {
+      chainId: 381660001,
       allowUnlimitedContractSize: true,
+      loggingEnabled: true,
+      accounts: { 
+        initialIndex: 0,
+        passphrase :`0xb584831ccaa00a44d3f76bd49743e2bc0caf5795376c6556bbd77bb1c6ebd4bf`
+      }
     },
   },
 };
